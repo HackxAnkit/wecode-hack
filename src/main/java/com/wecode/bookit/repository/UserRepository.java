62a801176b311adec51982e97cd1a172c2cf4c8b
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-27 05:26:50 +0530

    Added Role ENUM and Response DTO

--- src/main/java/com/wecode/bookit/repository/UserRepository.java
+++ src/main/java/com/wecode/bookit/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.wecode.bookit.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
+import java.util.UUID;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
