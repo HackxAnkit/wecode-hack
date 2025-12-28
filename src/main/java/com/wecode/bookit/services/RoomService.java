f8f3678cd126f9eeed1b2af7e561a186b9e96a37
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-28 17:26:43 +0530

    Updated Create room logic

--- src/main/java/com/wecode/bookit/services/RoomService.java
+++ src/main/java/com/wecode/bookit/services/RoomService.java
@@ -1,8 +1,19 @@
 package com.wecode.bookit.services;
 
+import com.wecode.bookit.dto.CreateMeetingRoomDto;
+import com.wecode.bookit.dto.MeetingRoomDto;
+import com.wecode.bookit.dto.UpdateMeetingRoomDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface RoomService {
-    public void addRoom(int id,String roomName, int seatingCapacity);
-    public void updateRoom(int roomId, String roomName, int seatingCapacity);
-    public void deleteRoom(int roomId);
-    public void getAllRooms();
+    MeetingRoomDto createRoom(CreateMeetingRoomDto createRoomDto);
+    MeetingRoomDto updateRoom(UpdateMeetingRoomDto updateRoomDto);
+    MeetingRoomDto getRoomById(UUID roomId);
+    List<MeetingRoomDto> getAllRooms();
+    void deleteRoom(UUID roomId);
+
+
 }
+
