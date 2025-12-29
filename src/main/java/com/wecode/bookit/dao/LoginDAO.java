5d743f802c22cf82c3abf07e5492d51264602603
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-30 04:54:28 +0530

    removed unwanted files

--- src/main/java/com/wecode/bookit/dao/LoginDAO.java
+++
@@ -1,8 +0,0 @@
-package com.wecode.bookit.dao;
-
-import com.wecode.bookit.model.Users;
-
-public interface LoginDAO {
-    Users authenticate(String email, String password);
-    void resetCredits(Users user);
-}
