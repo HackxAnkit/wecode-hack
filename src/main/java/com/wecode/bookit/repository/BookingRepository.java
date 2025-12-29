880188b2b45d268d487bd570191c28eb310340fc
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-30 04:46:46 +0530

    Created Manager controller & repository

--- src/main/java/com/wecode/bookit/repository/BookingRepository.java
+++ src/main/java/com/wecode/bookit/repository/BookingRepository.java
@@ -4,7 +4,18 @@ import com.wecode.bookit.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    List<Booking> findByUserId(UUID userId);
+    List<Booking> findByRoomId(UUID roomId);
+    Optional<Booking> findByBookingId(UUID bookingId);
+    List<Booking> findByUserIdAndStatus(UUID userId, String status);
+    List<Booking> findByUserIdAndMeetingDate(UUID userId, LocalDate meetingDate);
+    List<Booking> findByUserIdAndMeetingDateAndStatus(UUID userId, LocalDate meetingDate, String status);
 }
+
